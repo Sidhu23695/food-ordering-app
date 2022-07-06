261c95de0b5eba5b56d1592344dca591ba139473
Sidharth <sidharthshn@gmail.com>
2022-07-07 00:09:37 +0530

    slight update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Food App Coming Soon
+          Coming Soon - Food App
         </p>
       </header>
     </div>
