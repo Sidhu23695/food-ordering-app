abf8d6c84b20e8aa441691194691fac58b6d755a
Sidharth <sidharthshn@gmail.com>
2022-07-08 01:45:59 +0530

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Link
 } from "react-router-dom";
+import { ButtonGroup, Button } from 'react-bootstrap'
 
 import OrderDetails from './components/OrderDetails/OrderDetails';
 import MenuDetails from './components/MenuDetails/MenuDetails';
 import HomePage from './components/HomePage/HomePage';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App(props) {
+
+  // const currentPath = window.location.pathname;
+  const [currentPath, updateCurrentPath] = useState(window.location.pathname);
+  const buttonDetails = [
+    {
+      id: 1,
+      link: '/food-ordering-app',
+      title: 'Home',
+      variant: 'primary'
+    },
+    {
+      id: 2,
+      link: '/menu',
+      title: 'Menu Card',
+      variant: 'success'
+    },
+    {
+      id: 3,
+      link: '/order-details',
+      title: 'Order Details',
+      variant: 'warning'
+    }
+  ]
+  const exactPath = (path) => {
+    return (path === currentPath) ? true : false
+  }
+  const buttonGenerator =  (button) => {
+      return !exactPath(button.link) ? 
+      <Link to={button.link}><Button variant={button.variant} onClick={() => updateCurrentPath(button.link)}>{button.title}</Button></Link>
+      : <Button variant={button.variant} disabled={true}>{button.title}</Button>
+  }
 
   return (
     <Router>
       <div className="App">
-        <Link to="/food-ordering-app">Home</Link> <br/>
-        <Link to="/menu">Menu Card</Link> <br/>
-        <Link to="/order-details">Order Details</Link> <br/>
+        <ButtonGroup aria-label="Basic example">
+          {buttonDetails.map((button, i) => buttonGenerator(button))}
+        </ButtonGroup>
         <Routes>
           <Route path="/food-ordering-app" element={<HomePage {...props} />} />
           <Route path="/menu" element={<MenuDetails {...props} />} />
